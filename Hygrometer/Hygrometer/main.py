e0dabb9d4a29253c3d9a6040e867b7dc02414985
Dmytro Buzalo <dmytro.buzalo@namely.com>
2020-02-24 21:18:28 +0200

    Encapsulated logic of Model selection in the one procedure

--- Hygrometer/Hygrometer/main.py
+++ Hygrometer/Hygrometer/main.py
@@ -21,47 +21,48 @@ def any_msg(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
-   # Если сообщение из чата с ботом
    if call.message:
-      global model      
+          
       if call.data == "vit1":                                    
-         print(str(datetime.datetime.now()), " ", str(call.message.message_id),"  ", "Model = VIT1")
-         model = 1
-         bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Please enter Dry measure of VIT1")
-         bot.register_next_step_handler(call.message, SetDryTemp)
+         SetModel(call.message, model = 1)         
       elif call.data == "vit2":                           
-         print(str(datetime.datetime.now()), " ", str(call.message.message_id),"  ", "Model = VIT2")
-         model = 2
-         bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Please enter Dry measure of VIT2")
-         bot.register_next_step_handler(call.message, SetDryTemp)
+         SetModel(call.message, model = 2)        
+
+def SetModel(message, model):
+   global _model  
+   _model = model
+   print(str(datetime.datetime.now()), " ", str(message.message_id),"  ", "Model = ", _model)
+   bot.edit_message_text(chat_id = message.chat.id, message_id = message.message_id, text = "Please enter Dry measure of VIT" + str(_model))
+   bot.register_next_step_handler(message, SetDryTemp)
 
 def SetDryTemp(message):   
-   global dryTemp
+   global _dryTemp
    if message.text.isnumeric() == False:
       bot.send_message(message.from_user.id, "The value is not numeric. Please enter correct Dry measure") 
       print(str(datetime.datetime.now()), " ", "Incorrect value - ", message.text)
       bot.register_next_step_handler(message, SetDryTemp)  
    else:
-      dryTemp = round(float(message.text), 2)
-      print(str(datetime.datetime.now()), " ", str(message.message_id),"  ", "DryTemp = ", str(dryTemp))
-      bot.send_message(message.from_user.id, "Please enter Wet measure of VIT"+str(model))  
+      _dryTemp = round(float(message.text), 2)
+      print(str(datetime.datetime.now()), " ", str(message.message_id),"  ", "DryTemp = ", str(_dryTemp))
+      bot.send_message(message.from_user.id, "Please enter Wet measure of VIT"+str(_model))  
       bot.register_next_step_handler(message, SetWetTemp)  
 
 def SetWetTemp(message):   
-   global wetTemp
+   global _wetTemp
    global RelativeHumidity
    if message.text.isnumeric() == False:
       bot.send_message(message.from_user.id, "The value is not numeric. Please enter correct Wet measure")
       print(str(datetime.datetime.now()), " ", "Incorrect value - ", message.text)
       bot.register_next_step_handler(message, SetWetTemp)  
-   else:
-      wetTemp = round(float(message.text), 2)   
-      print(str(datetime.datetime.now()), " ", str(message.message_id),"  ", "WetTemp = ", str(wetTemp))
+   else:     
       try:
+         _wetTemp = round(float(message.text), 2)   
+         print(str(datetime.datetime.now()), " ", str(message.message_id),"  ", "WetTemp = ", str(_wetTemp))
          Hygrom1 = Hygrometer(Amendment1, HumidityTable1)
-         RelativeHumidity = round(Hygrom1.GetRelativeHumidity(dryTemp, wetTemp), 2)     
+         RelativeHumidity = round(Hygrom1.GetRelativeHumidity(_dryTemp, _wetTemp), 2)     
          bot.send_message(message.from_user.id, 'Relative Humidity is:  '+ str(RelativeHumidity) + '%')  
          print(str(datetime.datetime.now()), " ", str(message.message_id),"  ", "Relative Humidity is", str(RelativeHumidity), '%')
       except Exception as e:
          print(str(datetime.datetime.now()), "  ", "EXCEPTION: error during RelativeHumidity calculating")
+
 bot.polling(none_stop = True)
