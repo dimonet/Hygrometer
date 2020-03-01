0a663650d12352de497bdb625eba81dd446f44a0
Dmytro Buzalo <dmytro.buzalo@namely.com>
2020-03-01 23:12:38 +0200

    Changed to Webhook

--- Hygrometer/Hygrometer/main.py
+++ Hygrometer/Hygrometer/main.py
@@ -1,13 +1,61 @@
 # Bot's name - 'Hygrometerbot' 
 # User name 'hygrometerbot'
-import telebot
+import logging
+import ssl
 import datetime
 from Hygrometer import Hygrometer
 from DataStorage.Amendments import Amendment1
 from DataStorage.HumidityTables import HumidityTable1
-from telebot import types
+
+from aiohttp import web
+
+import telebot
+
+API_TOKEN = '925726841:AAEclsVk5bYXshpcMdCOflM1kCzjEpx2nbU'
+
+WEBHOOK_HOST = '34.70.66.245'
+WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
+WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
+
+WEBHOOK_SSL_CERT = '/home/dimont_mail/keys/url_cert.pem'  # Path to the ssl certificate
+WEBHOOK_SSL_PRIV = '/home/dimont_mail/keys/url_private.key'  # Path to the ssl private key
+#WEBHOOK_SSL_CERT = './keys/url_cert.pem'  # Path to the ssl certificate
+#WEBHOOK_SSL_PRIV = './keys/url_private.key'  # Path to the ssl private key
+
+# Quick'n'dirty SSL certificate generation:
+#
+# openssl genrsa -out webhook_pkey.pem 2048
+# openssl req -new -x509 -days 3650 -key webhook_pkey.pem -out webhook_cert.pem
+#
+# When asked for "Common Name (e.g. server FQDN or YOUR name)" you should reply
+# with the same value in you put in WEBHOOK_HOST
+
+WEBHOOK_URL_BASE = "https://{}:{}".format(WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/{}/".format(API_TOKEN)
+
+logger = telebot.logger
+telebot.logger.setLevel(logging.INFO)
+
+bot = telebot.TeleBot(API_TOKEN)
+
+app = web.Application()
+
+
+# Process webhook calls
+async def handle(request):
+    if request.match_info.get('token') == bot.token:
+        request_body_dict = await request.json()
+        update = telebot.types.Update.de_json(request_body_dict)
+        bot.process_new_updates([update])
+        return web.Response()
+    else:
+        return web.Response(status=403)
+
+
+app.router.add_post('/{token}/', handle)
+
+
 print(str(datetime.datetime.now()), "Telegram bot (v1.1) has started")
-bot = telebot.TeleBot("925726841:AAEclsVk5bYXshpcMdCOflM1kCzjEpx2nbU")
 
 def isFloat(string):
     try:
@@ -90,4 +138,22 @@ def SetWetTemp(message):
       except Exception as e:
          print(str(datetime.datetime.now()), "  ", "EXCEPTION: error during RelativeHumidity calculating")
 
-bot.polling(none_stop = True)
+
+# Remove webhook, it fails sometimes the set if there is a previous webhook
+bot.remove_webhook()
+
+# Set webhook
+bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
+                certificate=open(WEBHOOK_SSL_CERT, 'r'))
+
+# Build ssl context
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+context.load_cert_chain(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV)
+
+# Start aiohttp server
+web.run_app(
+    app,
+    host=WEBHOOK_LISTEN,
+    port=WEBHOOK_PORT,
+    ssl_context=context,
+)
